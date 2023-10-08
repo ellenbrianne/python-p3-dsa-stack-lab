79f53e5c2fad1fae27bfad3d034eb4d4935961da
Ellen Bennett <bennette831@gmail.com>
2023-10-07 21:34:52 -0600

    create isEmpty, pop, peek, full, and search methods

--- lib/Stack.py
+++ lib/Stack.py
@@ -5,22 +5,30 @@ class Stack:
         self.limit = limit
 
     def isEmpty(self):
-        pass
+        return True if not self.items else False
 
     def push(self, item):
-        self.items.append(item)
+        self.items.append(item) if len(self.items) < self.limit else None
 
     def pop(self):
-        pass
+        if self.items:
+            return self.items.pop()
+        else:
+            return None
 
     def peek(self):
-        pass
+        return self.items[len(self.items) - 1]
     
     def size(self):
         return len(self.items)
 
     def full(self):
-        pass
+        return True if self.limit == len(self.items) else False
 
     def search(self, target):
-        pass
+        search = -1
+        if target in self.items:
+            search = (len(self.items) - 1) - self.items.index(target)
+            return search
+        else:
+            return search
